888db14043df416b1ddf2f4e230d6141d362a548
Nathan Piel <nthnpiel@gmail.com>
2022-05-03 12:29:32 -0400

    Rearranged the function. added some forward decls
    Now there are no more warnings about undefined functions

--- chip8-assembler.lisp
+++ chip8-assembler.lisp
@@ -27,71 +27,201 @@
     (mapcar #'make-sexp
             (remove-blank (uiop/stream:read-file-lines x)))))
 
-(defun make-env (&optional outer)
-  (let ((inner (make-hash-table)))
-    (setf (gethash 'EQ inner) #'emit-op)
-    (setf (gethash 'NEQ inner) #'emit-op)
-    (setf (gethash 'SET inner) #'emit-op)
-    (setf (gethash 'ADD inner) #'emit-op)
-    (setf (gethash 'OR inner) #'emit-op)
-    (setf (gethash 'AND inner) #'emit-op)
-    (setf (gethash 'XOR inner) #'emit-op)
-    (setf (gethash 'SUB inner) #'emit-op)
-    (setf (gethash 'SHR inner) #'emit-op)
-    (setf (gethash 'SUBR inner) #'emit-op)
-    (setf (gethash 'SHL inner) #'emit-op)
-    (setf (gethash 'RAND inner) #'emit-op)
-    (setf (gethash 'DRAW inner) #'emit-op)
-    (setf (gethash 'BCD inner) #'emit-op)
-    (setf (gethash 'WRITE inner) #'emit-op)
-    (setf (gethash 'READ inner) #'emit-op)
-    (setf (gethash 'CLEAR inner) #'emit-op)
-    (setf (gethash 'RET inner) #'emit-op)
-    (setf (gethash 'CALL inner) #'emit-op)
-    (setf (gethash 'JUMP inner) #'emit-op)
-    (setf (gethash 'JUMP0 inner) #'emit-op)
-    (setf (gethash '+ inner) #'+)
-    (setf (gethash '- inner) #'-)
-    (setf (gethash '* inner) #'*)
-    (setf (gethash '/ inner) #'/)
-    (setf (gethash '% inner) #'mod)
-    (setf (gethash '&& inner) #'logand)
-    (setf (gethash '|| inner) #'logior)
-    (setf (gethash '^ inner) #'logxor)
-    (setf (gethash '<< inner) #'ash)
-    (setf (gethash '>> inner) #'(lambda (x y) (ash x (- y))))
-    (setf (gethash 'pow inner) #'expt)
-    (setf (gethash 'min inner) #'min)
-    (setf (gethash 'max inner) #'max)
-    (setf (gethash '< inner) #'(lambda (x y) (if (< x y) 1 0)))
-    (setf (gethash '<= inner) #'(lambda (x y) (if (<= x y) 1 0)))
-    (setf (gethash '== inner) #'(lambda (x y) (if (= x y) 1 0)))
-    (setf (gethash '!= inner) #'(lambda (x y) (if (/= x y) 1 0)))
-    (setf (gethash '>= inner) #'(lambda (x y) (if (>= x y) 1 0)))
-    (setf (gethash '> inner) #'(lambda (x y) (if (> x y) 1 0)))
-    (setf (gethash '~ inner) #'lognot)
-    (setf (gethash '! inner) #'(lambda (x) (if (zerop x) 1 0)))
-    (setf (gethash 'sin inner) #'sin)
-    (setf (gethash 'cos inner) #'cos)
-    (setf (gethash 'tan inner) #'tan)
-    (setf (gethash 'exp inner) #'exp)
-    (setf (gethash 'log inner) #'log)
-    (setf (gethash 'abs inner) #'abs)
-    (setf (gethash 'sqrt inner) #'sqrt)
-    (setf (gethash 'sign inner) #'(lambda (x) (if (plusp x) 1 -1)))
-    (setf (gethash 'ceil inner) #'ceiling)
-    (setf (gethash 'floor inner) #'floor)
-    
+(defun chip8-eval-v? (exp)
+  (match exp
+    ('V0 0) ('V1 1) ('V2 2) ('V3 3)
+    ('V4 4) ('V5 5) ('V6 6) ('V7 7)
+    ('V8 8) ('V9 9) ('VA #xA) ('VB #xB)
+    ('VC #xC) ('VD #xD) ('VE #xE) ('VF #xF)
+    (t nil)))
 
-    (setf (gethash 'BREAK inner) (lambda () '(BREAK)))
+(defun v? (exp)
+  (not (null (chip8-eval-v? exp))))
 
-    (list #x200 inner outer)))
+(defun builtin-var? (exp)
+  (or (eq exp 'KEY)
+      (eq exp 'ST)
+      (eq exp 'DT)
+      (eq exp 'I)))
+
+(defun self-evaluating? (exp)
+  (and (not (listp exp))
+       (or (numberp exp)
+           (builtin-var? exp)
+           (v? exp))))
 
 (defun chip8-type (exp)
   (cond
     ((v? exp) 'V)
     ((builtin-var? exp) exp)
     (t 'N)))
+
+(defun ins? (exp)
+  (and (listp exp)
+       (match (first exp)
+         ('EQ 't) ('NEQ 't) ('SET 't) ('ADD 't)
+         ('OR 't) ('AND 't) ('XOR 't) ('SUB 't)
+         ('SHR 't) ('SUBR 't) ('SHL 't) ('RAND 't)
+         ('DRAW 't) ('BCD 't) ('WRITE 't) ('READ 't)
+         ('CLEAR 't) ('RET 't) ('CALL 't) ('JUMP 't)
+         ('JUMP0 't)
+         (_ nil))))
+
+(defun def? (exp)
+  (and (listp exp)
+       (eq (first exp) 'DEF)))
+
+(defun label? (exp)
+  (and (listp exp)
+       (eq (first exp) 'LAB)))
+
+(defun loop? (exp)
+  (and (listp exp)
+       (eq (first exp) 'LOOP)))
+
+(defun application? (exp)
+  (and (not (null exp))
+       (listp exp)
+       (not (def? exp))
+       (not (label? exp))))
+
+(defun var? (exp)
+  (and (not (application? exp))
+       (not (self-evaluating? exp))))
+
+(defun include? (exp)
+  (and (listp exp)
+       (eq (first exp) 'INCLUDE)))
+
+(defun unpack? (exp)
+  (and (listp exp)
+       (eq (first exp) 'UNPACK)))
+
+(defun macro? (exp)
+  (and (listp exp)
+       (eq (first exp) 'MACRO)))
+
+(defun rotate-main (exps)
+  "Ensures that the code above 'lab main' is always at the end"
+  (let ((main-label (position '(lab main) exps :test #'equal)))
+    (if main-label
+        ;; defs should be kept at the beginning
+        ;; everything else should be put at the end
+        (let ((before-main
+                (reduce (lambda (a b)
+                          (if (or (def? a) (macro? a))
+                              (push a (first b))
+                              (push a (second b)))
+                          b)
+                        (reverse (nthcdr (- (length exps) main-label) (reverse exps)))
+                        :initial-value (list nil nil)
+                        :from-end t)))
+          (append (car before-main)
+                  (nthcdr main-label exps)
+                  (cdr before-main)))
+        (error "put main pls"))))
+
+;; These functions have circular depedencies to chip8-eval
+(declaim (ftype function
+                chip8-eval-application
+                chip8-eval-include
+                chip8-eval-ins
+                chip8-eval-loop
+                chip8-eval-macro
+                chip8-eval-unpack))
+
+(defun chip8-eval (exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((def? exp) (chip8-eval-def exp env))
+        ((label? exp) (chip8-eval-label exp env))
+        ((var? exp) (chip8-eval-var exp env))
+        ((loop? exp) (chip8-eval-loop exp env))
+        ((include? exp) (chip8-eval-include exp env))
+        ((unpack? exp) (chip8-eval-unpack exp env))
+        ((macro? exp) (chip8-eval-macro exp env))
+        ((ins? exp) (chip8-eval-ins exp env))
+        ((application? exp) (chip8-eval-application exp env))
+        (t (error "wow you did something bad"))))
+
+(defun process-labels (exps env)
+  "Flattens list and processes unresolved labels"
+  (cond
+    ((null exps) nil)
+    ((atom exps) (list exps))
+    (t (mapcan (lambda (x) (process-labels (chip8-eval x env) env)) exps))))
+
+(defun chip8-eval-file (exps env)
+  (cond
+    ((null exps) nil)
+    (t (append (chip8-eval (car exps) env)
+               (chip8-eval-file (rest exps) env)))))
+
+(defun chip8-eval-def (exp env)
+  (setf (gethash (cadr exp) (cadr env)) (caddr exp))
+  nil)
+
+(defun chip8-eval-label (exp env)
+  (setf (gethash (cadr exp) (cadr env)) (car env))
+  nil)
+
+(defun chip8-eval-var (exp env)
+  (let ((inner (gethash exp (cadr env)))
+        (outer (caddr env)))
+    (cond (inner inner)
+          (outer (chip8-eval-var exp outer))
+          (t exp))))
+
+(defun chip8-eval-macro (exp env)
+  (let ((name (cadr exp))
+        (args (caddr exp))
+        (body (cdddr exp)))
+    (setf (gethash name (cadr env))
+          (eval `(lambda (&rest vars)
+                   (let ((inner-env (make-env ',(copy-list env))))
+                     (mapcar (lambda (arg var)
+                               (setf (gethash arg (cadr inner-env)) var))
+                             ',args vars)
+                     (chip8-eval-file ',body inner-env))))))
+  nil)
+
+(defun chip8-eval-top (exps env)
+  (process-labels (chip8-eval-file (rotate-main exps) env) env))
+
+(defun chip8-eval-unpack (exp env)
+  ;; TODO: doesn't handle 16-bit addresses
+  (let ((nibble (chip8-eval (second exp) env))
+        (label (chip8-eval (third exp) env)))
+    (chip8-eval-file
+     `((set v0 ,(logior (ash nibble 4) (ash label -8)))
+       (set v1 ,(logand label #xFF)))
+     env)))
+
+(defun chip8-eval-args-partial (args env &key eval-v)
+  (mapcar (lambda (x)
+            (if (and (v? x) eval-v)
+                (chip8-eval-v? x)
+                (chip8-eval x env)))
+          args))
+
+(defun chip8-eval-ins (exp env)
+  (funcall (chip8-eval (first exp) env)
+           (first exp)
+           (chip8-eval-args-partial (rest exp) env)
+           env))
+
+(defun chip8-eval-include (exp env)
+  ;; TODO: error if number is larger than a byte
+  (incf (car env) (length (remove-if-not #'numberp exp)))
+  (chip8-eval-args-partial
+   (if (= (mod (length (rest exp)) 2) 0)
+       (rest exp)
+       (append (rest exp) '(0)))
+   env))
+
+(defun chip8-eval-application (exp env)
+  ;; Do I want to eval v registers here???
+  (apply (chip8-eval (first exp) env)
+         (chip8-eval-args-partial (rest exp) env :eval-v t)))
 
 (defun combine-op (args shell&info)
   (let ((shell (car shell&info))
@@ -156,73 +286,6 @@
            ('(JUMP N) '(#x1000 #x0))
            ('(JUMP0 N) '(#xB000 #x0))
            (_ '(0 0)))))))
-    
-(defun ins? (exp)
-  (and (listp exp)
-       (match (first exp)
-         ('EQ 't) ('NEQ 't) ('SET 't) ('ADD 't)
-         ('OR 't) ('AND 't) ('XOR 't) ('SUB 't)
-         ('SHR 't) ('SUBR 't) ('SHL 't) ('RAND 't)
-         ('DRAW 't) ('BCD 't) ('WRITE 't) ('READ 't)
-         ('CLEAR 't) ('RET 't) ('CALL 't) ('JUMP 't)
-         ('JUMP0 't)
-         (_ nil))))
-
-(defun chip8-eval-ins (exp env)
-  (funcall (chip8-eval (first exp) env)
-           (first exp)
-           (chip8-eval-args-partial (rest exp) env)
-           env))
-
-(defun chip8-eval-args-partial (args env &key eval-v)
-  (mapcar (lambda (x)
-            (if (and (v? x) eval-v)
-                (chip8-eval-v? x)
-                (chip8-eval x env)))
-          args))
-
-(defun chip8-eval-v? (exp)
-  (match exp
-    ('V0 0) ('V1 1) ('V2 2) ('V3 3)
-    ('V4 4) ('V5 5) ('V6 6) ('V7 7)
-    ('V8 8) ('V9 9) ('VA #xA) ('VB #xB)
-    ('VC #xC) ('VD #xD) ('VE #xE) ('VF #xF)
-    (t nil)))
-
-(defun v? (exp)
-  (not (null (chip8-eval-v? exp))))
-
-(defun builtin-var? (exp)
-  (or (eq exp 'KEY)
-      (eq exp 'ST)
-      (eq exp 'DT)
-      (eq exp 'I)))
-
-(defun self-evaluating? (exp)
-  (and (not (listp exp))
-       (or (numberp exp)
-           (builtin-var? exp)
-           (v? exp))))
-
-(defun def? (exp)
-  (and (listp exp)
-       (eq (first exp) 'DEF)))
-  
-(defun chip8-eval-def (exp env)
-  (setf (gethash (cadr exp) (cadr env)) (caddr exp))
-  nil)
-
-(defun label? (exp)
-  (and (listp exp)
-       (eq (first exp) 'LAB)))
-
-(defun chip8-eval-label (exp env)
-  (setf (gethash (cadr exp) (cadr env)) (car env))
-  nil)
-
-(defun loop? (exp)
-  (and (listp exp)
-       (eq (first exp) 'LOOP)))
 
 (defun chip8-eval-loop (exp env)
   (let ((label (list (first env)))
@@ -234,124 +297,67 @@
            :else
              :collect x)
      (emit-op 'JUMP label env))))
-  
-(defun var? (exp)
-  (and (not (application? exp))
-       (not (self-evaluating? exp))))
 
-(defun chip8-eval-var (exp env)
-  (let ((inner (gethash exp (cadr env)))
-        (outer (caddr env)))
-    (cond (inner inner)
-          (outer (chip8-eval-var exp outer))
-          (t exp))))
+(defun make-env (&optional outer)
+  (let ((inner (make-hash-table)))
+    (setf (gethash 'EQ inner) #'emit-op)
+    (setf (gethash 'NEQ inner) #'emit-op)
+    (setf (gethash 'SET inner) #'emit-op)
+    (setf (gethash 'ADD inner) #'emit-op)
+    (setf (gethash 'OR inner) #'emit-op)
+    (setf (gethash 'AND inner) #'emit-op)
+    (setf (gethash 'XOR inner) #'emit-op)
+    (setf (gethash 'SUB inner) #'emit-op)
+    (setf (gethash 'SHR inner) #'emit-op)
+    (setf (gethash 'SUBR inner) #'emit-op)
+    (setf (gethash 'SHL inner) #'emit-op)
+    (setf (gethash 'RAND inner) #'emit-op)
+    (setf (gethash 'DRAW inner) #'emit-op)
+    (setf (gethash 'BCD inner) #'emit-op)
+    (setf (gethash 'WRITE inner) #'emit-op)
+    (setf (gethash 'READ inner) #'emit-op)
+    (setf (gethash 'CLEAR inner) #'emit-op)
+    (setf (gethash 'RET inner) #'emit-op)
+    (setf (gethash 'CALL inner) #'emit-op)
+    (setf (gethash 'JUMP inner) #'emit-op)
+    (setf (gethash 'JUMP0 inner) #'emit-op)
+    (setf (gethash '+ inner) #'+)
+    (setf (gethash '- inner) #'-)
+    (setf (gethash '* inner) #'*)
+    (setf (gethash '/ inner) #'/)
+    (setf (gethash '% inner) #'mod)
+    (setf (gethash '&& inner) #'logand)
+    (setf (gethash '|| inner) #'logior)
+    (setf (gethash '^ inner) #'logxor)
+    (setf (gethash '<< inner) #'ash)
+    (setf (gethash '>> inner) #'(lambda (x y) (ash x (- y))))
+    (setf (gethash 'pow inner) #'expt)
+    (setf (gethash 'min inner) #'min)
+    (setf (gethash 'max inner) #'max)
+    (setf (gethash '< inner) #'(lambda (x y) (if (< x y) 1 0)))
+    (setf (gethash '<= inner) #'(lambda (x y) (if (<= x y) 1 0)))
+    (setf (gethash '== inner) #'(lambda (x y) (if (= x y) 1 0)))
+    (setf (gethash '!= inner) #'(lambda (x y) (if (/= x y) 1 0)))
+    (setf (gethash '>= inner) #'(lambda (x y) (if (>= x y) 1 0)))
+    (setf (gethash '> inner) #'(lambda (x y) (if (> x y) 1 0)))
+    (setf (gethash '~ inner) #'lognot)
+    (setf (gethash '! inner) #'(lambda (x) (if (zerop x) 1 0)))
+    (setf (gethash 'sin inner) #'sin)
+    (setf (gethash 'cos inner) #'cos)
+    (setf (gethash 'tan inner) #'tan)
+    (setf (gethash 'exp inner) #'exp)
+    (setf (gethash 'log inner) #'log)
+    (setf (gethash 'abs inner) #'abs)
+    (setf (gethash 'sqrt inner) #'sqrt)
+    (setf (gethash 'sign inner) #'(lambda (x) (if (plusp x) 1 -1)))
+    (setf (gethash 'ceil inner) #'ceiling)
+    (setf (gethash 'floor inner) #'floor)
+    
 
-(defun application? (exp)
-  (and (not (null exp))
-       (listp exp)
-       (not (def? exp))
-       (not (label? exp))))
+    (setf (gethash 'BREAK inner) (lambda () '(BREAK)))
 
-(defun chip8-eval-application (exp env)
-  ;; Do I want to eval v registers here???
-  (apply (chip8-eval (first exp) env)
-         (chip8-eval-args-partial (rest exp) env :eval-v t)))
-
-(defun include? (exp)
-  (and (listp exp)
-       (eq (first exp) 'INCLUDE)))
-
-(defun chip8-eval-include (exp env)
-  ;; TODO: error if number is larger than a byte
-  (incf (car env) (length (remove-if-not #'numberp exp)))
-  (chip8-eval-args-partial
-   (if (= (mod (length (rest exp)) 2) 0)
-       (rest exp)
-       (append (rest exp) '(0)))
-   env))
-
-(defun unpack? (exp)
-  (and (listp exp)
-       (eq (first exp) 'UNPACK)))
-
-(defun chip8-eval-unpack (exp env)
-  ;; TODO: doesn't handle 16-bit addresses
-  (let ((nibble (chip8-eval (second exp) env))
-        (label (chip8-eval (third exp) env)))
-    (chip8-eval-file
-     `((set v0 ,(logior (ash nibble 4) (ash label -8)))
-       (set v1 ,(logand label #xFF)))
-     env)))
-
-(defun macro? (exp)
-  (and (listp exp)
-       (eq (first exp) 'MACRO)))
-
-(defun chip8-eval-macro (exp env)
-  (let ((name (cadr exp))
-        (args (caddr exp))
-        (body (cdddr exp)))
-    (setf (gethash name (cadr env))
-          (eval `(lambda (&rest vars)
-                   (let ((inner-env (make-env ',(copy-list env))))
-                     (mapcar (lambda (arg var)
-                               (setf (gethash arg (cadr inner-env)) var))
-                             ',args vars)
-                     (chip8-eval-file ',body inner-env))))))
-      nil)
-
-(defun process-labels (exps env)
-  "Flattens list and processes unresolved labels"
-  (cond
-    ((null exps) nil)
-    ((atom exps) (list exps))
-    (t (mapcan (lambda (x) (process-labels (chip8-eval x env) env)) exps))))
-
-(defun rotate-main (exps)
-  "Ensures that the code above 'lab main' is always at the end"
-  (let ((main-label (position '(lab main) exps :test #'equal)))
-    (if main-label
-        ;; defs should be kept at the beginning
-        ;; everything else should be put at the end
-        (let ((before-main
-                (reduce (lambda (a b)
-                          (if (or (def? a) (macro? a))
-                              (push a (first b))
-                              (push a (second b)))
-                          b)
-                        (reverse (nthcdr (- (length exps) main-label) (reverse exps)))
-                        :initial-value (list nil nil)
-                        :from-end t)))
-          (append (car before-main)
-                  (nthcdr main-label exps)
-                  (cdr before-main)))
-        (error "put main pls"))))
-
-(defun chip8-eval-top (exps env)
-  (process-labels (chip8-eval-file (rotate-main exps) env) env))
-
-(defun chip8-eval-file (exps env)
-  (cond
-    ((null exps) nil)
-    (t (append (chip8-eval (car exps) env)
-               (chip8-eval-file (rest exps) env)))))
-
-(defun chip8-err (exp)
-  (format t "You typed: ~a~%That was bad~%" exp))
-
-(defun chip8-eval (exp env)
-  (cond ((self-evaluating? exp) exp)
-        ((def? exp) (chip8-eval-def exp env))
-        ((label? exp) (chip8-eval-label exp env))
-        ((var? exp) (chip8-eval-var exp env))
-        ((loop? exp) (chip8-eval-loop exp env))
-        ((include? exp) (chip8-eval-include exp env))
-        ((unpack? exp) (chip8-eval-unpack exp env))
-        ((macro? exp) (chip8-eval-macro exp env))
-        ((ins? exp) (chip8-eval-ins exp env))
-        ((application? exp) (chip8-eval-application exp env))
-        (t (chip8-err exp))))
-
+    (list #x200 inner outer)))
+    
 (defun chip8-compile (file)
   (chip8-eval-top
    (parse (tokenize file))
@@ -364,14 +370,3 @@
                      :if-does-not-exist :create
                      :element-type 'unsigned-byte)
     (mapcar (lambda (x) (write-byte x f)) bytes)))
-
-;;(defun main ()
-;;  (cond
-;;    ((>= (length *posix-argv*) 2)
-;;     (format t "~a~%"
-;;     (chip8-eval-top
-;;       (parse (tokenize (cadr *posix-argv*)))
-;;       (make-env))))
-;;    (t nil)))
-
-;;(main)
