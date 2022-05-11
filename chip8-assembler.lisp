4de97b0fef7f081d9c8bf5d77b06a11cf618e91f
Nathan Piel <nthnpiel@gmail.com>
2022-05-10 22:38:54 -0400

    fix bug
    the '1' should not have been there, it is not needed anymore

--- chip8-assembler.lisp
+++ chip8-assembler.lisp
@@ -299,7 +299,7 @@
            ('(EQ V N) '(#x4000 #x8))
            ('(EQ V KEY) '(#xE0A1 #x8))
            
-           ('(NEQ V KEY) '(#xE09E #x81))
+           ('(NEQ V KEY) '(#xE09E #x8))
            ('(NEQ V V) '(#x5000 #x48))
            ('(NEQ V N) '(#x3000 #x8))
            
